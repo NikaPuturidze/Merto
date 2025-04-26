291c1771d121f1d4e53eccd038db282685a465d0
Nika Puturidze <nikushaputuridze@gmail.com>
2025-04-26 22:01:08 +0400

    Feat: Add footer

--- webapp/src/index/index.js
+++ webapp/src/index/index.js
@@ -7,8 +7,9 @@ class Index {
       .then((response) => response.json())
       .then((data) => {
         const currentLang = data[this.currentLang]
-        this.setHeader(currentLang)
-        this.setNav(currentLang)
+        this.setHeader(currentLang.header)
+        this.setNav(currentLang.header)
+        this.setFooter(currentLang.footer)
       })
 
     fetch('.mega-menu.json') // Will be added actual endpoint in future
@@ -19,11 +20,11 @@ class Index {
       })
   }
 
-  setHeader(data) {
-    const header = document.querySelector('.header-container')
+  setHeader(header) {
+    const headerCont = document.querySelector('.header-container')
     const ul = document.createElement('ul')
 
-    data.languages.languages.forEach((language) => {
+    header.languages.languages.forEach((language) => {
       const li = document.createElement('li')
       li.classList.add(`dropdown-${language.language.toLowerCase()}`)
 
@@ -34,12 +35,12 @@ class Index {
       ul.appendChild(li)
     })
 
-    header.innerHTML = `
+    headerCont.innerHTML = `
       <div class="logo">
-        <img src="https://merto-be87.kxcdn.com/merto/wp-content/uploads/2024/09/logo-electronic.png" />
+        <img src="${header.logoUrl}" />
       </div>
       <div class="header-center">
-        <input type="text" placeholder="${data.searchPlaceholder}" />
+        <input type="text" placeholder="${header.searchPlaceholder}" />
         <div class="search-button">
           <i class="fi fi-rr-search"></i>
         </div>
@@ -47,8 +48,8 @@ class Index {
       <div class="header-right">
         <div class="container">
           <div class="header-language">
-            <img src="${data.languages.flagUrl}" />
-            <span>${data.languages.language}</span>
+            <img src="${header.languages.flagUrl}" />
+            <span>${header.languages.language}</span>
             <i class="fa-solid fa-chevron-down"></i>
             <div class="language-dropdown-container">
               <div class="language-dropdown">
@@ -59,7 +60,7 @@ class Index {
         </div>
         <div class="header-account">
           <i class="fi fi-rr-circle-user"></i>
-            <span>${data.account}</span>
+            <span>${header.account}</span>
         </div>
         <div class="header-favourite">
           <i class="fi fi-rr-heart"></i>
@@ -72,7 +73,7 @@ class Index {
       </div>
     `
 
-    data.languages.languages.forEach((language) => {
+    header.languages.languages.forEach((language) => {
       document.querySelector(`.dropdown-${language.language.toLowerCase()}`).addEventListener('click', () => {
         window.localStorage.setItem('lang', language.id)
         location.reload()
@@ -80,10 +81,86 @@ class Index {
     })
   }
 
-  setNav(data) {
-    const nav = document.querySelector('#cat-list')
-    data.categories.forEach((category) => {
-      nav.innerHTML += `
+  setFooter(footer) {
+    const footerCont = document.querySelector('.footer-container')
+    const bottomNav = document.createElement('div')
+    bottomNav.classList.add('bottom-nav')
+
+    footer.top.bottomNav.forEach((e) => {
+      let ul = document.createElement('ul')
+      e.forEach((e) => {
+        let li = document.createElement('li')
+        li.textContent = e
+        ul.appendChild(li)
+      })
+
+      bottomNav.appendChild(ul)
+    })
+
+    footerCont.innerHTML = `
+      <div class="top">
+        <div class="info">
+          <div class="logo">
+            <img src="${footer.top.info.logoUrl}" />
+          </div>
+          <address>
+            <i class="fa-solid fa-location-dot"></i>
+            <h4>${footer.top.info.address}</h4>
+          </address>
+          <div class="mail">
+            <i class="fa-solid fa-envelope"></i>
+            <a href="mailto:${footer.top.info.mail}">${footer.top.info.mail}</a>
+          </div>
+          <a href="tel:${footer.top.info.number}">${footer.top.info.number}</a>
+        </div>
+        ${bottomNav.outerHTML}
+        <div class="about">
+          <h3>${footer.top.about.title}</h3>
+          <form>
+            <input type="email" placeholder="email@example.com" required />
+            <button type="submit">
+              <i class="fa-solid fa-arrow-right"></i>
+            </button>
+          </form>
+          <div class="brands">
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-tiktok"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-reddit"></i>
+          </div>
+        </div>
+      </div>
+      <div class="middle">
+        <div class="cla">
+          <i class="fa-solid fa-phone"></i>
+          <div class="cta">
+            <h2>${footer.middle[0].cta}</h2>
+            <a href="mailto:contact@example.com">${footer.middle[0].referrer}</a>
+          </div>
+        </div>
+
+        <div class="cla">
+          <i class="fa-solid fa-face-smile"></i>
+          <div class="cta">
+            <h2>${footer.middle[1].cta}</h2>
+            <a href="">${footer.middle[1].referrer}</a>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="copyright">${footer.bottom.copyright}</div>
+        <div class="payment">
+          <img src="https://merto-be87.kxcdn.com/merto/wp-content/uploads/2024/04/payment-icons-1.png" />
+        </div>
+      </div>
+    `
+  }
+
+  setNav(nav) {
+    const navCont = document.querySelector('#cat-list')
+    nav.categories.forEach((category) => {
+      navCont.innerHTML += `
         <li>
           <img src="${category.imageUrl}" />
           <span>${category.name}</span>
@@ -91,9 +168,9 @@ class Index {
       `
     })
 
-    nav.innerHTML += `
+    navCont.innerHTML += `
     <li>
-      <span>${data.recentlyViewed}</span>
+      <span>${nav.recentlyViewed}</span>
       <i class="fa-solid fa-chevron-down"></i>
     </li>
   `
