10372436e5e4fcac2bde9d2808fbb4a015fa26a3
Nika Puturidze <nikushaputuridze@gmail.com>
2025-04-27 05:17:20 +0400

    Fix: Hotfix

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: 'src/index',
+  root: 'src',
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `@use "../styles.scss" as *;`,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        about: './catalog/about.html',
       },
     },
   },
